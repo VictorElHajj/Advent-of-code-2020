ff079abd1755f563ff8ec4ca267d60c00031fd94
MysticPing <hajjvictor@live.se>
2020-12-01 11:39:29 +0100

    Use binary search

--- src/day1.rs
+++ src/day1.rs
@@ -1,41 +1,33 @@
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
-
-#[aoc_generator(day1, part1)]
-pub fn generator_part1(input: &str) -> BinaryHeap<i32> {
-  input.lines().map(|l| l.parse::<i32>().unwrap()).collect()
-}
-
-#[aoc_generator(day1, part2)]
-pub fn generator_part2(input: &str) -> BinaryHeap<Reverse<i32>> {
-  input
-    .lines()
-    .map(|l| l.parse::<i32>().unwrap())
-    .map(|l| Reverse(l))
-    .collect()
+#[aoc_generator(day1)]
+pub fn generator_part1(input: &str) -> Vec<usize> {
+  let mut parsed: Vec<usize> = input.lines().map(|l| l.parse::<usize>().unwrap()).collect();
+  parsed.sort();
+  parsed
 }
 
 #[aoc(day1, part1)]
-pub fn part1(input: &BinaryHeap<i32>) -> Result<i32, &str> {
-  for (i, v1) in input.iter().enumerate() {
-    for v2 in input.iter().skip(i) {
-      if v1 + v2 == 2020 {
-        return Ok(v1 * v2);
-      }
-    }
+pub fn part1(input: &Vec<usize>) -> Result<usize, &str> {
+  for v1 in input.iter() {
+    let goal = 2020 - v1;
+    let search = input.binary_search(&goal);
+    match search {
+      Ok(i2) => return Ok(v1 * input[i2]),
+      _ => (),
+    };
   }
   Err("Solution not found")
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &BinaryHeap<Reverse<i32>>) -> Result<i32, &str> {
+pub fn part2(input: &Vec<usize>) -> Result<usize, &str> {
   for (i, v1) in input.iter().enumerate() {
-    for (j, v2) in input.iter().skip(i).enumerate() {
-      for v3 in input.iter().skip(j) {
-        if v1.0 + v2.0 + v3.0 == 2020 {
-          return Ok(v1.0 * v2.0 * v3.0);
-        }
-      }
+    for v2 in input.iter().skip(i) {
+      let goal = 2020 - v1 - v2;
+      let search = input.binary_search(&goal);
+      match search {
+        Ok(i2) => return Ok(v1 * v2 * input[i2]),
+        _ => (),
+      };
     }
   }
   Err("Solution not found")
