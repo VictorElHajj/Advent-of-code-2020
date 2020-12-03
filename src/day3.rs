a2100c6b7734d4b1a460f370a98dc5f4867e8f08
MysticPing <hajjvictor@live.se>
2020-12-03 14:58:40 +0100

    90% speedup by using mod instead of cycle

--- src/day3.rs
+++ src/day3.rs
@@ -4,8 +4,8 @@ pub enum Tile {
 }
 
 #[aoc_generator(day3)]
-pub fn generator(input: &str) -> Vec<Vec<Tile>> {
-  input
+pub fn generator(input: &str) -> (Vec<Vec<Tile>>, usize) {
+  let map = input
     .as_bytes()
     .split(|b| *b as char == '\n')
     .map(|r| {
@@ -17,13 +17,19 @@ pub fn generator(input: &str) -> Vec<Vec<Tile>> {
         })
         .collect::<Vec<Tile>>()
     })
-    .collect::<Vec<Vec<Tile>>>()
+    .collect::<Vec<Vec<Tile>>>();
+  let width = input.bytes().take_while(|b| *b as char != '\n').count();
+  (map, width)
 }
 
-pub fn toboggan(input: &Vec<Vec<Tile>>, slope: &(usize, usize)) -> u32 {
+pub fn toboggan(input: &Vec<Vec<Tile>>, slope: &(usize, usize), width: &usize) -> u32 {
   let mut trees = 0;
   for (height, row) in input.iter().enumerate().step_by(slope.1) {
-    trees += match row.iter().cycle().nth(slope.0 * height / slope.1).unwrap() {
+    trees += match row
+      .iter()
+      .nth((slope.0 * height / slope.1) % width)
+      .unwrap()
+    {
       Tile::Empty => 0,
       Tile::Tree => 1,
     }
@@ -32,17 +38,17 @@ pub fn toboggan(input: &Vec<Vec<Tile>>, slope: &(usize, usize)) -> u32 {
 }
 
 #[aoc(day3, part1)]
-pub fn part1(input: &Vec<Vec<Tile>>) -> u32 {
+pub fn part1(input: &(Vec<Vec<Tile>>, usize)) -> u32 {
   let slope = (3, 1);
-  toboggan(input, &slope)
+  toboggan(&input.0, &slope, &input.1)
 }
 
 #[aoc(day3, part2)]
-pub fn part2(input: &Vec<Vec<Tile>>) -> u32 {
-  let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+pub fn part2(input: &(Vec<Vec<Tile>>, usize)) -> u32 {
+  let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
   slopes
     .iter()
-    .map(|slope| toboggan(input, slope))
+    .map(|slope| toboggan(&input.0, slope, &input.1))
     .fold(1, |tot, trees| tot * trees)
 }
 
@@ -51,7 +57,8 @@ mod tests {
   use super::*;
   #[test]
   fn test_part2() {
-    let test_input = "..##.......
+    let test_input = "\
+..##.......
 #...#...#..
 .#....#..#.
 ..#.#...#.#
